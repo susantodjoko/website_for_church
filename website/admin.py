507dd3454ad443730ee704b6811cda7e5f163a23
susantoDjoko <djoko.susanto1979@gmail.com>
2026-05-11 15:25:43 +0700

    feat: register website models in Django admin with custom display

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import HeroSlide, SermonSeries, Sermon, Ministry, ServiceTime, Event, AboutPage
 
-# Register your models here.
+
+@admin.register(HeroSlide)
+class HeroSlideAdmin(admin.ModelAdmin):
+    list_display = ['headline', 'is_active']
+
+
+@admin.register(SermonSeries)
+class SermonSeriesAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Sermon)
+class SermonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'series', 'pastor', 'date', 'is_featured']
+    list_filter = ['series', 'topic']
+    search_fields = ['title', 'pastor']
+
+
+@admin.register(Ministry)
+class MinistryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'order']
+    ordering = ['order']
+
+
+@admin.register(ServiceTime)
+class ServiceTimeAdmin(admin.ModelAdmin):
+    list_display = ['campus_name', 'is_online']
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'time', 'is_published']
+    list_filter = ['is_published', 'date']
+
+
+@admin.register(AboutPage)
+class AboutPageAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return not AboutPage.objects.exists()
