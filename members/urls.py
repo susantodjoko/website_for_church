1942e60677329ec220cbdce2f01e1e29706dd090
susantoDjoko <djoko.susanto1979@gmail.com>
2026-05-11 16:25:44 +0700

    feat: add events, about and visit page templates

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from django.http import HttpResponse
 
 app_name = 'members'
 
-urlpatterns = []
+def placeholder(request):
+    return HttpResponse('Members area coming soon.')
+
+urlpatterns = [
+    path('', placeholder, name='member_list'),
+]
